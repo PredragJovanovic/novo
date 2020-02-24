4f4d0a665e238893993fd4b72f6b2d6230691392
Predrag jovanovic <predrag777777@gmail.com>
2020-02-24 12:03:26 +0100

    proba dodajem dva fajla header i novo

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<ul>
+		<li><a href="stranica.php">pocetna</a></li>
+		<li><a href="stranica2.php">druga</a></li>
+		<li><a href="stranica3.php">treca</a></li>
+	</ul>
+	
+</body>
+</html>
